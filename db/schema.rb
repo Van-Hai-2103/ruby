9ca3453ba38b82fc6a8264536c6e98d6969c2219
HaiLV <levanhai30023012@gmailcom>
2025-01-10 17:23:55 +0700

    lan9

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_01_10_095732) do
+ActiveRecord::Schema[8.0].define(version: 2025_01_10_095947) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -67,6 +67,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_01_10_095732) do
     t.string "key_hash"
   end
 
+  create_table "subscribers", force: :cascade do |t|
+    t.bigint "product_id", null: false
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_subscribers_on_product_id"
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "subscribers", "products"
 end
