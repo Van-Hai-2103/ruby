d7a8032a28eca32fa1c0b515d94a5d5df6bdbfc5
HaiLV <levanhai30023012@gmailcom>
2025-01-10 16:42:34 +0700

    lan6

---
+++ db/migrate/20250110094054_create_solid_cache_entries.rb
@@ -0,0 +1,9 @@
+class CreateSolidCacheEntries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :solid_cache_entries do |t|
+      t.string :key
+      t.text :value
+      t.timestamps
+    end
+  end
+end
